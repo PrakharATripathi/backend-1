b73182ccd200e349fae7f90d08e14e4d60063d93
PRAKHAR-A-TRIPATHI <151377167+PrakharATripathi@users.noreply.github.com>
2024-05-01 15:45:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,6 +10,12 @@ app.get('/', (req, res) => {
 app.get('/x', (req, res) => {
     res.send("new name");
 })
+app.get('/github', (req, res) => {
+    res.json(
+        {
+        "employee":{"name":"John", "age":30, "city":"New York"}
+        });
+})
 
 app.listen(process.env.PORT,()=>{
     console.log(`server start you can go ${port}`)
